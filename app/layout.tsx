957583e9cf29e29b456653c37f7e2b5180989dd4
osemensilas <osemensilas@gmail.com>
2026-04-06 11:55:06 +0100

    update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Header from "@/components/sections/Headereader";
+import Header from "@/components/sections/Header.tsx";
 import Head from "next/head";
 
 export const metadata: Metadata = {
